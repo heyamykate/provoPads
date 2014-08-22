02c1631937a1d97879f2d8ed8f7d3a15de005232
Amy Andrews <amy.kate.andrews@gmail.com>
2014-08-22 10:52:49 -0600

    adding apts json file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('angularPadsApp').controller('MainCtrl', function ($scope, $firebase) {
-  var ref = new Firebase("https://intense-fire-6764.firebaseio.com");
+  var ref = new Firebase('https://intense-fire-6764.firebaseio.com');
 
   var sync = $firebase(ref);
     // download the data into a local object
   var syncObject = sync.$asObject();
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
+  syncObject.$bindTo($scope, 'data');
 
   var userId = sessionStorage.userId;
   if(!userId) {
     userId = + new Date();
     sessionStorage.userId = userId;
-  };
+  }
 
   $scope.saveAnswer = function(label, value) {
     if(!$scope.data.users) {
@@ -27,6 +27,6 @@ angular.module('angularPadsApp').controller('MainCtrl', function ($scope, $fireb
       $scope.data.users[userId].answers = {};
     }
     $scope.data.users[userId].answers[label] = value;
-  }
+  };
 
   });
